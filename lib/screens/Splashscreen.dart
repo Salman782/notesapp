01b6ebd22e08d96c7f8e03b7f25a8262d3d39334
Salman782 <shonamunda38@gmail.com>
2021-01-08 10:31:12 +0500

    splash screen

---
+++ lib/screens/Splashscreen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:notesapp/main.dart';
+
+import 'home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 20),
+            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      body: Center(
+        child: Image.asset('assets/bs.jpg'),
+      ),
+    );
+  }
+}
